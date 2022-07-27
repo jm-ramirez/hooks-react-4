e5d95038b764cdc14ba17cb9922dbd2e58b2eba8
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2022-07-27 12:21:26 -0300

    I add onChange method.

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -18,32 +18,33 @@ export const MyForm = () => {
     const sent = (e) => {
         e.preventDefault();
 
-        // let course = {
-        //     title: e.target.title.value,
-        //     year: e.target.year.value,
-        //     description: e.target.description.value,
-        //     autor: e.target.autor.value,
-        //     email: e.target.email.value
-        // };
-
         let course = serializerForm(e.target);
 
         setForm(course);
     }
 
+    const changed = ({ target }) => {
+        const { name, value } = target;
+
+        setForm({
+            ...form,
+            [name]: value
+        })
+    };
+
     return (
         <div>
             <h1>Form</h1>
             <p>Form to save a course</p>
-            <p>Course saved:</p>
-            <pre>{JSON.stringify(form)}</pre>
+            <p>Course saved: {form.title}</p>
+            <pre className='code'>{JSON.stringify(form)}</pre>
 
             <form onSubmit={sent} className='my-form'>
-                <input type='text' name='title' placeholder='Title' />
-                <input type='number' name='year' placeholder='Year of publication' />
-                <textarea name='description' placeholder='Description' />
-                <input type='text' name='autor' placeholder='Autor' />
-                <input type='email' name='email' placeholder='Email' />
+                <input type='text' name='title' onChange={changed} placeholder='Title' />
+                <input type='number' name='year' onChange={changed} placeholder='Year of publication' />
+                <textarea name='description' onChange={changed} placeholder='Description' />
+                <input type='text' name='autor' onChange={changed} placeholder='Autor' />
+                <input type='email' name='email' onChange={changed} placeholder='Email' />
 
                 <input type='submit' value='Send' />
             </form>
